cf761ec5cbd6aacebdb6deeacb417b7db0ba159d
RDT69 <josediaz@gmail.com>
2024-01-27 03:11:07 +0000

    cambios de ultima hora

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var express =  require('express');
+var express = require('express');
+let cors = require('cors');
+var morgan = require('morgan');
+
+const PIZZAS = require('./pizzas');
+
 var app = express();
+app.use(cors());
+app.use(morgan('dev'));
+
+const PIZZAS = [
+    { id: 1, name: "Barbacoa" },
+    { id: 2, name: "Carbonara" },
+    { id: 3, name: "Bacon Crispy" },
+];
 
 const PORT = 3000;
 app.listen(PORT, () => {
@@ -11,14 +24,55 @@ app.get('/', (req, res) => {
     res.send("Hola mundo");
 });
 
+app.use (express.json());
+
+// app.get('/api/telepizza', (req, res) => {
+//     res.json({
+//         success: true,
+//         Message: "Listado de pizzas",
+//         data: {
+//         count: PIZZAS.length,
+//         pizzas: PIZZAS
+//     }
+//     });
+// });
+
 app.get('/api/telepizza', (req, res) => {
-    res.send("api de telepi con método GET");
-});
+    let pizzas_ingredientes = PIZZAS.map(pizza =>{
+        return {
+            id: pizza.id,
+            nombre: pizza.nombre,
+        }
+    })
+    res.json({
+        success: true,
+        Message: "Listado de pizzas",
+        data: {
+            count: pizzas_ingredientes.length,
+            pizzas: pizzas_ingredientes
+            }
+    })
+
+})
 
 app.use(express.static('public'));
 
 
 app.get('/api/telepizza/:id', (req, res) => {
-   let id = req.params.id;
-   res.send("Pizza con id: " + id); 
+    let id = req.params.id;
+    let filtro = PIZZAS.filter(pizza=>pizza.id==id);
+    if(filtro.length>0){
+        res.json({
+            success: true,
+            message: "Pizza encontrada con id: " + id,
+            data: filtro[0]
+        });
+    } else {
+        res.status(404).json({
+            success: false,
+            error_code: 4321,
+            message: "No se encuentra ninguna pizza con id: " + id,
+        });
+    }
 });
+
